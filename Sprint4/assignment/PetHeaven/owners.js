b63b841091a4d1f3ca9994300fb54b5ad7d53cbd
Ece Aytürk <75135337+eceayturk@users.noreply.github.com>
2024-05-22 16:43:13 +0300

    Update owners.js

--- Sprint4/assignment/PetHeaven/owners.js
+++ Sprint4/assignment/PetHeaven/owners.js
@@ -1,26 +1,18 @@
 $(document).ready(function() {
 
     function fetchSpeakerData(file) {
-      $.getJSON("json_data/" + file + ".json", function(data) {
-        
-        $(".dialog").empty();
-  
-        data.owners.forEach(function(speaker) {
-          var title = $("<h1>").text(speaker.title);
-          var image = $("<img>").attr("src", speaker.image);
-          var month = $("<h2>").html(speaker.month + "<br>" + speaker.speaker);
-          var description = $("<p>").text(speaker.text);
-  
-          $(".dialog").append(title, image, month, description);
+        $.getJSON("json_data/" + file + ".json", function(data) {
+            var speaker = data.owners[0]; // assuming the JSON structure has an array named 'owners'
+            $("#title").text(speaker.title);
+            $("#image").attr("src", speaker.image);
+            $("#month").html(speaker.month + "<br>" + speaker.speaker);
+            $("#description").text(speaker.text);
         });
-      });
     }
-  
+
     $("#nav_list a").click(function(e) {
-  
-  
-      var title = $(this).attr("title");
-  
-      fetchSpeakerData(title);
+        e.preventDefault(); // Prevent default link behavior
+        var title = $(this).attr("title");
+        fetchSpeakerData(title);
     });
-  });
+});
